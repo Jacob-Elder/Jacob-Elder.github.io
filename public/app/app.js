b9818dce71f867a2e82707618d2fcfb274b49811
Jacob-Elder <jacobelderonly@gmail.com>
2016-11-19 19:09:43 -0800

    building up basic file structure

---
+++ public/app/app.js
@@ -0,0 +1,12 @@
+angular.module("MyWebpage", ["ui.router"])
+.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+			$urlRouterProvider.otherwise("/");
+
+	$stateProvider
+	.state("home", {
+		url: "/",
+		templateUrl: "app/views/home.html"
+	})
+
+	$locationProvider.html5Mode(true);
+}])
